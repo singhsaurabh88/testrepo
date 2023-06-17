d876fe72f86af431c2bae8ac7c652f23f99fa612
singhsaurabh88 <109754949+singhsaurabh88@users.noreply.github.com>
2023-06-17 16:58:16 -0400

    soda

--- p1.py
+++ p1.py
@@ -1,7 +1,8 @@
-hungry=input("are you hungry")
+hungry=input("are you hungry:")
 if hungry=="yes":
     print("eat samose")
 else:
     thirsty= input("are you thirsty:")
     if thirsty=="yes":
         print("drink water")
+        print("drink soda")
